a81ceca21c4b427239c834b2f672b7d3744c21f1
sowmik-sec <sowmik.sec@gmail.com>
2023-02-19 20:51:00 +0600

    some components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./components/About";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Products from "./components/Products";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
